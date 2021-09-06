fde28176bd57ed74d944c522a00239a23304ccd4
Anne Raaijman <anneraaijman@hotmail.com>
2021-09-06 12:18:56 +0200

    added title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Map from "./components/map";
+import Map from "./components/Map";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
+      <Title />
+
       <Map />
     </div>
   );
---
+++ src/components/Title.js
@@ -0,0 +1,7 @@
+export default function Title() {
+  return (
+    <div>
+      <h1>Fruits</h1>
+    </div>
+  );
+}
