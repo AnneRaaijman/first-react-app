3dac2fcd595e7202cce0fc67c5f71d8b5482dc1e
Anne Raaijman <anneraaijman@hotmail.com>
2021-09-06 11:27:26 +0200

    first deployment

---
+++ src/components/Map.js
@@ -0,0 +1,29 @@
+export default function Map() {
+  const data = [
+    {
+      name: "Banana",
+    },
+    {
+      name: "Apple",
+    },
+    {
+      name: "Orange",
+    },
+    {
+      name: "Watermelon",
+    },
+  ];
+  return (
+    <div>
+      {!data
+        ? "Loading"
+        : data.map((fruit) => {
+            return (
+              <div>
+                <p>{fruit.name}</p>
+              </div>
+            );
+          })}
+    </div>
+  );
+}
